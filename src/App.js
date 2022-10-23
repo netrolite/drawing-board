4025e8bce043cf1724fad3a23d360ba1151cb9be
matvey733 <morozovmatvej75@gmail.com>
2022-10-23 11:10:56 +0300

    Still slow but uses a different algorithm

--- src/App.js
+++ src/App.js
@@ -8,27 +8,23 @@ export default function App() {
     // hover handler for each box
     function handleHover(id, bgColor) {
         setBoxes(prevState => {
-            return prevState.map(item => {
-                if(item.id === id) {
-                    let newColor;
-                    if(item.color) newColor = undefined;
-                    else newColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-                    return { ...item, color: newColor };
-                };
-                return { ...item };
-            })
+            const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16)
+            bgColor ? prevState[id].color = undefined : prevState[id].color = randomColor;
+            return [...prevState]
         })
     }
 
     // creating an array of all the boxes
-    const boxesElems = boxes.map(item => (
-        <Box 
-            key={item.id}
-            handleHover={handleHover}
-            id={item.id}
-            color={item.color}
-        />
-    )) 
+    const boxesElems = boxes.map(item => {
+        return (
+            <Box 
+                key={item.id}
+                handleHover={handleHover}
+                id={item.id}
+                color={item.color}
+            />
+        )
+    }) 
 
     return (
         <main className='main'>
